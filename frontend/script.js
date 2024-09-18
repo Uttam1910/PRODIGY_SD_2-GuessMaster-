4e37e7f8d114a35d2a1a1a1abdfec258db610560
Uttam1910 <uttamt1202@gmail.com>
2024-09-18 19:44:34 +0530

    changed

--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,3 @@
-// async function submitGuess() {
-//     let guess = document.getElementById('guessInput').value;
-//     let response = await fetch('http://127.0.0.1:5000/guess', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({ guess: guess }),
-//     });
-//     let data = await response.json();
-//     document.getElementById('feedback').innerText = data.result;
-//     document.getElementById('attempts').innerText = data.attempts;
-// }
-
 let secretNumber = generateRandomNumber();
 let attempts = 0;
 let bestScore = localStorage.getItem('bestScore') || null;
